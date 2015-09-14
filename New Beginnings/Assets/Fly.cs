c3f7a03a3c75bd84893b36e08b011689a29e7368
Zachary Roddy <zackztr@gmail.com>
2015-09-14 15:28:12 -0600

    no message

---
+++ New Beginnings/Assets/Fly.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly : MonoBehaviour 
+{
+	private float heightTest = 3f;
+	void Start ()
+	{
+		Debug.Log(transform);
+		if(transform.position.y <= heightTest)
+		{/*
+			Debug.Log("I'm about to hit the ground!");
+			FlyAway()
+		*/
+		}
+		else if(transform.position.y >= heightTest)
+		{
+			Debug.Log("I'm flying!");
+		}
+	}
+	private void FlyAway()
+	{
+		Debug.Log ("Fly Away!");
+		Vector3 newPosition = new Vector3(0,7,0);
+		transform.position = newPosition;
+	}
+}
+//Single Line comment
+
+/*
+ * Multi Line
+ * 2
+ * 3
+ */
+

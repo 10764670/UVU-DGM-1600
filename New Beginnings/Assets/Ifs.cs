02a535864cc4309cdd99317694af0e9c1dae683a
Zachary Roddy <zackztr@gmail.com>
2015-09-28 14:52:20 -0600

    no message

---
+++ New Beginnings/Assets/Ifs.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ifs : MonoBehaviour 
+{
+
+	public float LemonsEnough = 5;
+	public float LemonsTooMany = 20;
+	public float Lemons = 0;
+
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			LemonsTest();
+			Lemons = Lemons + 1;
+		}
+	}
+
+	void LemonsTest ()
+	{
+		if (Lemons == LemonsEnough) 
+		{
+			Debug.Log ("When life gives you lemons, make lemonade.");
+		} else if (Lemons == LemonsTooMany) 
+		{
+			Debug.Log ("On second thought...");
+		} else 
+		{
+			Debug.Log ("Added a lemon");
+		}
+	}
+}

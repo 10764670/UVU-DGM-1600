be2b012cbe8ff5ce3a7e30e0722d6a9434994b7b
Zachary Roddy <zackztr@gmail.com>
2015-10-26 15:16:24 -0600

    no message

---
+++ New Beginnings/Assets/For.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class For : MonoBehaviour {
+
+	int Lemons = 4;
+	public float NumA = 4;
+	public float NumB = 0;
+
+	void Start () 
+	{
+		string[] strings = new string[1];
+
+		strings [0] = "1";
+
+		foreach (string item in strings) {
+			print (item);
+		}
+
+		for (int i = 0; i < Lemons; i++) {
+			print ("Printing line#" + i);
+			NumB++;
+		}
+		if (NumB == NumA) 
+		{
+			print ("Line1");
+			print ("Line2");
+			print ("Line3");
+		}
+	}
+	
+	void Update () {
+	
+	}
+
+}
---
+++ New Beginnings/Assets/While.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class While : MonoBehaviour 
+{
+
+	int Lemons = 1;
+
+	void Start () 
+	{
+		while (Lemons > 0) {
+			print ("Line1");
+			Lemons--;
+		}
+
+		do {
+			print ("Line2");
+			Lemons--;
+		} while (Lemons == 0);
+	}
+
+	void Update () {
+	
+	}
+}
